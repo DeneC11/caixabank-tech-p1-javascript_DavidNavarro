5afc45038e6c5996971f61c59b1c1fca613869c4
dnc11 <35237286+dnc11@users.noreply.github.com>
2026-03-17 18:09:23 +0100

    Vinculamos la API y vemos que datos nos da

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+const API_URL = "https://pokeapi.co/api/v2/pokemon?limit=50"
+
+async function loadPokemons() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    console.log(data);
+}
+
 console.log("Pokédex iniciada");
+loadPokemons();
